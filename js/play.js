d7e22eb5611a24209ba8f6ee08de75c58fd23223
Yuta Koshizawa <koher@koherent.org>
2018-07-20 23:39:39 +0900

    Implement the spinner in play.html

--- js/play.js
+++ js/play.js
@@ -1,7 +1,7 @@
 $(function() {
   var editor = $(".sq-editor");
   var first = true;
-  var inputArea = $(".sq-input>textarea", editor)[0];
+  var inputArea = $(".sq-input>.sq-mirror>textarea", editor)[0];
   var inputMirror = CodeMirror.fromTextArea(inputArea, {
     lineNumbers: true,
     lineWrapping: true,
@@ -17,7 +17,7 @@ $(function() {
       first = false;
     }
   });
-  var outputArea = $(".sq-output>textarea", editor)[0];
+  var outputArea = $(".sq-output>.sq-mirror>textarea", editor)[0];
   var outputMirror = CodeMirror.fromTextArea(outputArea, {
     lineNumbers: false,
     lineWrapping: true,
@@ -25,25 +25,33 @@ $(function() {
     readOnly: true,
     theme: "output",
   });
+  var outputSpinner = $(".sq-output>.sq-spinner", editor);
   var playButton = $(".sq-play-button");
   var closeButton = $(".sq-close-button");
   playButton.click(function() {
+    outputMirror.setValue("");
+    outputSpinner.css({display: "flex"});
     playButton.attr("disabled", true);
     editor.addClass("sq-show-output");
     playButton.addClass("sq-show-output");
     closeButton.addClass("sq-show-output");
+    var defaultOutput = "ここに結果が表示されます。";
     var source = inputMirror.getValue();
     SwiftQuest.postSource(source).done(function(result) {
         if (result.status == 'success') {
             outputMirror.setValue(result.result);
-        } if (result.status == 'timeout') {
-            alert('エラーが発生しました。プログラムの実行にかかる時間が長すぎます。');
+        } else if (result.status == 'timeout') {
+            outputMirror.setValue(defaultOutput);
+            alert('プログラムの実行にかかる時間が長すぎるため、中断されました。');
         } else if (result.status == 'noSource') {
+            outputMirror.setValue(defaultOutput);
             alert('システムエラーが発生しました。');
         }
     }).fail(function() {
+        outputMirror.setValue(defaultOutput);
         alert('ネットワークエラーが発生しました。');
     }).always(function() {
+        outputSpinner.css({display: "none"});
         playButton.attr("disabled", false);
     });
   });
